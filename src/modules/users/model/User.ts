d0488668b6db688750b73f9871e98c6dee042e4f
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2022-12-21 23:07:31 -0300

    Implemented entity design

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,24 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id?: string;
+  name: string;
+  email: string;
+  admin: boolean;
+  created_at: Date;
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+      this.admin = false;
+    }
+
+    const dateTimeNow = new Date();
+
+    this.created_at = dateTimeNow;
+    this.updated_at = dateTimeNow;
+  }
 }
 
 export { User };
