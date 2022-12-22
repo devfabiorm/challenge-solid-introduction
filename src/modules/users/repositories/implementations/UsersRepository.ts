10f6c3d8434acbe5aba75e5a9007d3fada40527f
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2022-12-21 23:07:47 -0300

    Implemented repository methods

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,40 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user: User) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user: User) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const indexOfUser = this.users.findIndex(
+      (user: User) => user.id === receivedUser.id
+    );
+
+    this.users[indexOfUser].admin = true;
+    this.users[indexOfUser] = receivedUser;
+    this.users[indexOfUser].updated_at = new Date();
+
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
